808515690397b7d2a14d197530b3385bf5bc46e9
abhisheksonkar3738 <166475861+abhisheksonkar3738@users.noreply.github.com>
2024-09-23 12:04:22 +0530

    Create 2-days basic.js

---
+++ 2-days basic.js
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        /*
+
+        var a=10;
+        console.log(a);
+        var a=20;
+        console.log(a);        // re declare allow using var key
+      */
+// ==========
+        // let a=30;
+        // console.log(a);
+        // let a=20;
+        // console.log(a);    // it declare only one time;
+
+
+// =============================================================================
+
+    // a=20;
+    // console.log(a);
+    // var a;
+
+
+// =======
+
+//    b=30;
+//    console.log(b);
+//    let b;                   // declare variable using let before use
+
+
+
+//    ===================================================================================
+
+//  var a=10;
+//  console.log(a);
+//  {
+//     var a=20;
+//     console.log(a);
+
+//  }
+
+//  console.log(a);
+
+//  =============
+
+//    let c=10;
+//    console.log(c);
+//    {
+//         let c=40;
+//         console.log(c);
+//    }
+//    console.log(c);
+
+//    ==========================================================================
+
+
+    // const pi=3.14;
+    // console.log(pi);
+
+    // pi=34.54;
+    // console.log(pi);
+
+    </script>
+</body>
+</html>
