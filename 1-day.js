dddf19fd16d74adbc21961c36b2e895381b2292b
abhisheksonkar3738 <166475861+abhisheksonkar3738@users.noreply.github.com>
2024-09-23 12:01:42 +0530

    Basic and typeof method

---
+++ 1-day.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   
+</head>
+<body>
+   <script >
+
+    var name="kavita";
+    var designation="admin";
+    var salary=45600;
+    var city="indore";
+    var fun=function()
+    {
+    };
+
+    var arr=[];
+    var flag=true;
+
+    document.write("hello i am "+name+" i am "+designation+" from "+city+" my salary is "+salary);
+    document.write("<br>");
+    document.write(name+" : "+typeof(name));
+    document.write("<br>");
+    document.write(designation+" : "+typeof(designation));
+    document.write("<br>");
+    document.write(salary+" : "+typeof(salary));
+    document.write("<br>");
+    document.write(city+" : "+typeof(city));
+    document.write("<br>");
+    document.write(flag+" : "+typeof(flag));
+
+   </script> 
+</body>
+</html>
